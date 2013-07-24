87c981a6baacc7da301f3e6c40fb3d378847bcc7
DeltaCephei <deornorth@gmail.com>
2013-07-24 17:35:04 +1200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyWorld::Application.config.secret_key_base = '69121f0ef99bdc7f15b5cebe9c452bae2ff4e2d470dc7594e84b1257137b8ea7f58a5f205473068d793bab92c45e94dabb388ecb2f9fe164b6a7e8663474862d'
